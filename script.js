e6c6653ae30873413f3381b92955960e76fb541b
Abydenes <schoolreef2@protonmail.com>
2022-07-22 15:18:37 +0300

    Done except prompting from user at every round
    Created all the functions needed but a single user input is
    used in all the 5 rounds.

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+//let playerSelection = prompt("rock paper scissors?".toLowerCase());
+let playerSelection = 'rock';
+let computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     let randomizer = Math.floor(Math.random() * 3);
     switch (randomizer) {
         case 0:
-            return 'Rock';
+            return 'rock';
             break;
         case 1:
-            return 'Paper';
+            return 'paper';
             break;
         case 2:
-            return 'Scissors';
+            return 'scissors';
             break;
         default:
             break;
@@ -19,23 +20,33 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerSelectionInsens = playerSelection.toLowerCase();
-    let computerSelectionInsens = computerSelection.toLowerCase();
-
-    if (playerSelectionInsens === computerSelectionInsens) {
-        return "It's a draw!"
-    } else if (playerSelectionInsens === 'rock') {
-        if (computerSelectionInsens === 'paper') {
-            return 'You lose! Paper beats Rock!';
-        } else return 'You win! Rock beats Scissors!';
-    } else if (playerSelectionInsens === 'paper') {
-        if (computerSelectionInsens === 'scissors') {
-            return 'You lose! Scissors beats Paper!';
-        } else return 'You win! Paper beats Rock!';
-    } else if (playerSelectionInsens === 'scissors') {
-        if (computerSelectionInsens === 'rock') {
-            return 'You lose! Rock beats scissors!';
-        } else return 'You win! Scissors beats Paper!';
-    }
+    if (playerSelection == computerSelection) return 'Draw!';
+    if (playerSelection == 'rock' && computerSelection == 'scissors') return 'Player won!';
+    else if (playerSelection == 'paper' && computerSelection == 'rock') return 'Player won!';
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') return 'Player won!';
+    else return 'Computer won!';
 }
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = playRound(playerSelection, computerSelection);
+        computerSelection = getComputerChoice();
+        console.log(`Round: ${i} => ${result}`);
+
+        if (result == 'Player won!') {
+            playerScore++
+        } else if (result == 'Computer won!') {
+            computerScore++
+        };
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Player won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer won the game!");
+    } else console.log("DRAW!")
+}
+
+game();
