2633f0d70216f02659a679052c3dee831a0e614b
Abydenes <schoolreef2@protonmail.com>
2022-07-22 15:26:56 +0300

    Prompt user to get input
    Added the feature to get an input from the user. The result of
    each round is shown on the console after getting the input from
    the user. After all rounds have been completed, the winner is
    announced.

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-//let playerSelection = prompt("rock paper scissors?".toLowerCase());
-let playerSelection = 'rock';
-let computerSelection = getComputerChoice();
+let playerSelection;
+let computerSelection;
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = playRound(playerSelection, computerSelection);
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("rock paper scissors?".toLowerCase());
+        console.log(`Round: ${i} => ${result}`);
+
+        if (result == 'Player won!') {
+            playerScore++
+        } else if (result == 'Computer won!') {
+            computerScore++
+        };
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Player won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer won the game!");
+    } else console.log("DRAW!")
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) return 'Draw!';
+    if (playerSelection == 'rock' && computerSelection == 'scissors') return 'Player won!';
+    else if (playerSelection == 'paper' && computerSelection == 'rock') return 'Player won!';
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') return 'Player won!';
+    else return 'Computer won!';
+}
 
 function getComputerChoice() {
     let randomizer = Math.floor(Math.random() * 3);
@@ -17,36 +47,6 @@ function getComputerChoice() {
         default:
             break;
     }
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) return 'Draw!';
-    if (playerSelection == 'rock' && computerSelection == 'scissors') return 'Player won!';
-    else if (playerSelection == 'paper' && computerSelection == 'rock') return 'Player won!';
-    else if (playerSelection == 'scissors' && computerSelection == 'paper') return 'Player won!';
-    else return 'Computer won!';
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(playerSelection, computerSelection);
-        computerSelection = getComputerChoice();
-        console.log(`Round: ${i} => ${result}`);
-
-        if (result == 'Player won!') {
-            playerScore++
-        } else if (result == 'Computer won!') {
-            computerScore++
-        };
-    }
-
-    if (playerScore > computerScore) {
-        console.log("Player won the game!");
-    } else if (computerScore > playerScore) {
-        console.log("Computer won the game!");
-    } else console.log("DRAW!")
 }
 
 game();
