08eb11d5bb347f6490e094a81479e99aa63257d7
abydenes <schoolreef2@protonmail.com>
2022-09-24 08:30:45 +0300

    Shorten code

--- script.js
+++ script.js
@@ -1,85 +1,53 @@
-let playerSelection;
-let computerSelection;
-let result = '';
-let computerScore = 0;
-let playerScore = 0;
-
-const rockButton = document.querySelector('.rock')
-rockButton.addEventListener('click', () => {
-    playRound('rock', computerSelection)
-    logScore();
-});
-
-const paperButton = document.querySelector('.paper')
-paperButton.addEventListener('click', () => {
-    playRound('paper', computerSelection)
-    logScore();
-});
-
-const scissorsButton = document.querySelector('.scissors')
-scissorsButton.addEventListener('click', () => {
-    playRound('scissors', computerSelection)
-    logScore();
-});
-
-
-
-function logScore() {
-    div.textContent = `computer = ${computerScore} player = ${playerScore}`
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3)
+  return randomNumber == 0 ? "rock" : randomNumber == 1 ? "paper" : "scissors"
 }
 
-const body = document.querySelector('body');
-const div = document.createElement('div');
-body.appendChild(div);
-
-const div2 = document.createElement('div')
-body.appendChild(div2);
-
+let playerScore = 0;
+let computerScore = 0;
+let result = "";
+const buttons = document.querySelectorAll('button');
+const div = document.querySelector(".result");
+let para = document.createElement("p");
+para.className = "para"
 
 function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
-    if (playerScore == 5 || computerScore == 5) {
-        if (computerScore == 5) {
-            div2.textContent = "Computer won the game!"
-            return
-        }
-        else if (playerScore == 5) { 
-            div2.textContent = "Player won the game!"
-            return 
-        }
-    }
-    if (playerSelection == computerSelection) result = 'Draw!';
-    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        result = 'Player won!';
-        playerScore++
-    }
-    else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        result = 'Player won!';
-        playerScore++
-    }
-    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        result = 'Player won!';
-        playerScore++
-    }
-    else {
-        result = 'Computer won!';
-        computerScore++
-    }
+  if (playerScore == 5) {
+    para.innerText = "Player won the game!";
+    return
+  } else if (computerScore == 5) {
+    para.innerText = "Computer won the game!";
+    return
+  }
+  if (playerSelection == computerSelection) result = "It's a draw";
+  else if ((playerSelection == 'rock' && computerSelection == 'scissors')
+    || (playerSelection == 'paper' && computerSelection == 'rock')
+    || (playerSelection == 'scissors' && computerSelection == 'paper')) {
+    result = 'Player won!';
+    playerScore++;
+  } else {
+    result = 'Computer won!';
+    computerScore++;
+  }
 }
 
-function getComputerChoice() {
-    let randomizer = Math.floor(Math.random() * 3);
-    switch (randomizer) {
-        case 0:
-            return 'rock';
-            break;
-        case 1:
-            return 'paper';
-            break;
-        case 2:
-            return 'scissors';
-            break;
-        default:
-            break;
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (button.id == "reset") {
+      playerScore = 0;
+      computerScore = 0;
+      result = "";
+      para.innerText = "";
+      displayScores();
+      return
     }
-}g
+    playRound(button.id, getComputerChoice());
+    displayScores();
+  });
+});
+
+function displayScores() {
+  div.innerText = `Player = ${playerScore} Computer = ${computerScore} \n ${result}`
+  div.appendChild(para)
+}
+
